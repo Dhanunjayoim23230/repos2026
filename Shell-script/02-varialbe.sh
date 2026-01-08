3e7874623a5a65e7a5174894f6ce8136ddbf5064
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-08 16:10:33 +0530

    variable conversation

---
+++ Shell-script/02-varialbe.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+#conversation by using varialbs.
+
+echo "vrushali: hi harsha?"
+echo "harsha: hi vrushali" 
+echo "vrushali: how are you harsha"
+echo "harsha: fine vrshali, how are you"
+echo "vrushali: good harsha"
