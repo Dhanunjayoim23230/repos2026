ea6fa6931ac7d5310aecc312e264febfc5eed6d7
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-12 17:38:30 +0530

    file exist

---
+++ Shell-script/b-nameinput.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Script to accept your name as input and print:
+#Hello <name>, welcome to shell scripting
+
+echo "Hello $1 , welcome to shell scripting"
---
+++ Shell-script/c-fileexist.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Write a script to check:
+# If a file exists
+# If it is readable, writable, executable
+
+find / -name "journey.txt"
