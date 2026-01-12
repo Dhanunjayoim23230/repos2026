58e1cc88d931cc9d6907f00a33691910aa5a6011
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-12 15:30:48 +0530

    shell variable practice 03-script

---
+++ Shell-script/03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: hi how are you $person2"
+echo "$person2: hi $person1, how are you"
+echo "$person1: good $person2, what are you doing"
+echo "$person2: good $person1 all good"
