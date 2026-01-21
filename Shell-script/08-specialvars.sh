5ac74ab841315c109cefa09ecd15ace16c78b6ac
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-21 21:45:06 +0530

    special variables

--- Shell-script/08-specialvars.sh
+++ Shell-script/08-specialvars.sh
@@ -8,11 +8,11 @@ echo "current script name: $0"
 echo "All variables passed: $@"
 echo "Number of variables passed: $#"
 
-
+echo "process id of last command in background: $!"
 
 sleep 6 &
 echo "process id of current script: $$"
 echo "exit status of previous: $?"
 
-echo "process id of last command in backend: $!"
+
 
