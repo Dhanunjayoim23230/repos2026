e6698d80e16e7079979dce489861aec1d1a0c489
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-21 20:11:39 +0530

    ARRAY SCRIPT ADDED

--- Shell-script/07-array.sh
+++ Shell-script/07-array.sh
@@ -2,7 +2,7 @@
 
 MOVIES=('SANKRNATI' 'RRR' 'NNM')
 
-echo "first movie: $MOVIES[0]"
+echo "first movie: ${MOVIES[0]}"
 echo "second movie:$MOVIES[1]"
 echo "third movie: $MOVIES[3]"
 echo "all movies passed: $MOVIES[@]"
