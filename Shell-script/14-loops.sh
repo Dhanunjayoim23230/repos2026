02297b86bb089d9f5e36ea3a3ad99b7908baed08
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-26 08:58:43 +0530

    loops

---
+++ Shell-script/14-loops.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+
+for i in {0...5}
+do
+    echo " print $i"
+done
+
+echo "********************************************************"
+
+for var in apple banana orange
+do
+    echo " print fruits name: $var"
+done
+
+echo "********************************************************"
+
+while [ i -eq 4 ]
+do
+    echo " Print while loop: $i "
+done
+
+echo "********************************************************"
+
+while read -r file
+do
+    echo "file content: $file"
+done < 13-logs.sh
+
