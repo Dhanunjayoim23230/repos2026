d06fe6b6a71f296446cb12dbd827033fd56c2afe
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-26 10:34:17 +0530

    loops

--- Shell-script/14-loops.sh
+++ Shell-script/14-loops.sh
@@ -58,11 +58,11 @@
 select colour in red orange green blue violet
 do
 echo " you choose colour here :: $colour "
-    if [ "#?" -eq 2 ]
+    if [ "$#" -eq 2 ]
     then
         echo " i selected colour :: $colour "
     fi
-    if [ "#?" -eq 1 ]
+    if [ "$#" -eq 1 ]
     then
         echo " i selected colur :: $colour"
     fi
