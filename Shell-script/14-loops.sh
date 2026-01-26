1c84ccd3e9661ab8e58d0b7ef11c426fbfad9039
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-26 10:02:21 +0530

    loops

--- Shell-script/14-loops.sh
+++ Shell-script/14-loops.sh
@@ -1,42 +1,42 @@
 #!bin/bash
 
-for i in {0..5}
-do
-    echo " print $i"
-done
-
-echo "********************************************************"
-
-for var in apple banana orange
-do
-    echo " print fruits name: $var"
-done
-
-echo "********************************************************"
-
-j=0
-
-while [ $j -le 4 ]
-do
-    echo " Print while loop: $j "
-    ((j++))
-done
-
-echo "********************************************************"
-
-# while read -r file
+# for i in {0..5}
 # do
-#     echo "file content: $file"
-# done < 13-logs.sh
+#     echo " print $i"
+# done
 
-echo "********************************************************"
-#Runs until condition become true(Inverse of while)
-i=1
-until [ $i -gt 5 ]
-do
-    echo "print until loop from 1 to 5 :$i "
-    ((i++))
-done
+# echo "********************************************************"
+
+# for var in apple banana orange
+# do
+#     echo " print fruits name: $var"
+# done
+
+# echo "********************************************************"
+
+# j=0
+
+# while [ $j -le 4 ]
+# do
+#     echo " Print while loop: $j "
+#     ((j++))
+# done
+
+# echo "********************************************************"
+
+# # while read -r file
+# # do
+# #     echo "file content: $file"
+# # done < 13-logs.sh
+
+# echo "********************************************************"
+# #Runs until condition become true(Inverse of while)
+# i=1
+# until [ $i -gt 5 ]
+# do
+#     echo "print until loop from 1 to 5 :$i "
+#     ((i++))
+# done
 
 echo "********************************************************"
 #executes until statement becomes true
