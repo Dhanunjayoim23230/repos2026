703f5073a044adfc9df5ed1a19c966c6332f226d
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-26 12:00:43 +0530

    loops_packages

--- Shell-script/16-delete-old-logs.sh
+++ Shell-script/16-delete-old-logs.sh
@@ -5,9 +5,11 @@ SOURCE_DIR="/home/ec2-user/shell_script_logs"
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +30)
 echo "$FILES_TO_DELETE"
 
-while read -r file
+while read -r files
 do
-    echo "print the files:: $file"
-    
-   # rm -rf $files
+    echo "print the files to delete :: $files"
+
+   rm -rf $files
+
+   echo "Files deleted :: $files "
 done <<< $FILES_TO_DELETE
