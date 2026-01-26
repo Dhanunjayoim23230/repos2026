e9c5811c70641dd720d558a3005fbbd75cc3e607
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-26 10:17:52 +0530

    loops

--- Shell-script/14-loops.sh
+++ Shell-script/14-loops.sh
@@ -58,5 +58,15 @@
 select colour in red orange green blue violet
 do
 echo" you choose colure :: $colour "
+    if [ $colour -eq orange ]
+    then
+        echo " i selected colour :: $colour "
+        break
+    fi
+    if [ $colour -eq red ]
+    then
+        echo " i selected colur :: $colour"
+        break
+    fi
 break
 done
