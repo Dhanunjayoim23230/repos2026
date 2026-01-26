1a2ff2a9bd0d04c1cc5af826b2f3e09d4897169c
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-26 17:52:51 +0530

    loops_packages

--- Shell-script/18-backup.sh
+++ Shell-script/18-backup.sh
@@ -1,5 +1,9 @@
 #!bin/bash
 
+#/home/ec2-user/source/app_logs
+#/home/ec2-user/dest/backup_logs
+
+
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=$(3:-14)
@@ -14,27 +18,27 @@ USAGE(){
 
 USAGE
 
-if [ -f $SOURCE_DIR ]
-then
-    echo "source directory is available"
-else
-    echo "source directory not available"
-    exit 1
-fi
+# if [ -f $SOURCE_DIR ]
+# then
+#     echo "source directory is available"
+# else
+#     echo "source directory not available"
+#     exit 1
+# fi
 
-if [ -f $DEST_DIR ]
-then
-    echo "destination directory is available"
-else
-    echo "destination directory not available"
-    exit 1
-fi
+# if [ -f $DEST_DIR ]
+# then
+#     echo "destination directory is available"
+# else
+#     echo "destination directory not available"
+#     exit 1
+# fi
 
-FIND_FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS )
+# FIND_FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS )
 
-if [ -f $FIND_FILES ]
-then
-    echo "backup eligible files:: $FIND_FILES "
-else
-    echo "No file to backup"
-fi
+# if [ -f $FIND_FILES ]
+# then
+#     echo "backup eligible files:: $FIND_FILES "
+# else
+#     echo "No file to backup"
+# fi
