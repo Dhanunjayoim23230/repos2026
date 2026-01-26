cf645a546dd0807259aaa751906bb5851a470f82
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-26 09:58:48 +0530

    loops

--- Shell-script/14-loops.sh
+++ Shell-script/14-loops.sh
@@ -39,9 +39,10 @@ do
 done
 
 echo "********************************************************"
+#executes until statement becomes true
 i=10
-until [ $i -lt 5 ]
+until [ $i -lt 11 ]
 do
-    echo "print until loop from 10 to 5 :$i "
+    echo "print until loop from 10 to 11 :$i "
     ((i++))
 done
