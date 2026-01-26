1e2fbc5a38c392f4993f2cbac0ec08f3173965f5
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-26 11:34:40 +0530

    loops

---
+++ Shell-script/15-loops_packeages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+CHECK_ROOT(){
+    if [ USER_ID -ne 0 ]
+    then
+        echo "YOU MUST NEED SUDO PERMISSIONS"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo " $2... :: SUCCESS "
+    else
+        echo " $2 ...:: FAILED"
+        exit 1
+    fi
+}
+
+
+for package in $@
+do
+    dnf install $package -y
+    VALIDATE $? "$package is installing"
+done
